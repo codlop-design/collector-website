24f6ff57d198f6535b357cec0fa25ca86c1e8175
mohamed eldrageny <mohamedeldrageny99@gmail.com>
2023-12-19 09:57:15 +0200

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -320,11 +320,43 @@ var incrementMinus = buttonMinus.click(function () {
     element.classList.toggle("active");
   }
 
-// lightgallary//
-lightGallery(document.getElementById('animated-thumbnails-gallery'), {
-    thumbnail: true,
-    plugins: [lgThumbnail,lgAutoplay,lgFullscreen,lgRotate,lgShare]
 
-});
+// button_menue
+$(document).ready(function(){
+    $(".btn_menu").click(function(e){
+        e.preventDefault()
+            $(".header__menu--navigation").addClass("show_menu");
+    
+    });
+  });
+
+//X_menu_Action
+
+$(document).ready(function(){
+    $(".btn_close").click(function(e){
+        e.preventDefault()
+            $(".header__menu--navigation").removeClass("show_menu");
+    
+    });
+  });
+
+// when click on background div of menue remove the show_menu
+  $(document).ready(function(){
+    $(".menu-backdrop").click(function(e){
+        e.preventDefault()
+            $(".header__menu--navigation").removeClass("show_menu");
+    
+    });
+  });
+
+
+  // lightgallary//
+ lightGallery(document.getElementById('animated-thumbnails-gallery'), {
+     thumbnail: true,
+     plugins: [lgThumbnail,lgAutoplay,lgFullscreen,lgRotate,lgShare]
+
+ });
+
+
 
 
